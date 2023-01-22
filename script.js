d2ee089310156ca2c1a43cc3cee00913b201c1e3
Mo <122757829+MoneebMog@users.noreply.github.com>
2023-01-21 21:23:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ function speak() {
   var text = document.getElementById("text").value;
   // Get the button
   var button = document.getElementById("speak-button");
-  // Disable the button
-  button.disabled = true;
   // Get the selected voice from the dropdown menu
   var voiceSelect = document.getElementById("voice-select");
   var voice = voiceSelect.value;
@@ -17,11 +15,6 @@ function speak() {
       utterance.voice = synth.getVoices().find(function(v) {
           return v.lang === voice;
       });
-  }
-  // Add an event listener to detect when speaking is done
-  utterance.addEventListener('end', function() {
-    // Enable the button when speaking is done
-    button.disabled = false;
-  });
+  };
   synth.speak(utterance);
 }

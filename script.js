3acff0a9957ec982bc152399e6123c97d9ae5623
Mo <122757829+MoneebMog@users.noreply.github.com>
2023-01-21 20:56:12 -0500

    Update script.js
    added more accents (accents in the first place)

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
 function speak() {
-    // Get the text from the textarea
-    var text = document.getElementById("text").value;
-    // Get the button
-    var button = document.getElementById("speak-button");
-    // Disable the button
-    button.disabled = true;
-  
-    // Use the SpeechSynthesis API to speak the text
-    var synth = window.speechSynthesis;
-    var utterance = new SpeechSynthesisUtterance(text);
-    // Add an event listener to detect when speaking is done
-    utterance.addEventListener('end', function() {
-      // Enable the button when speaking is done
-      button.disabled = false;
-    });
-    synth.speak(utterance);
+  // Get the text from the textarea
+  var text = document.getElementById("text").value;
+  // Get the button
+  var button = document.getElementById("speak-button");
+  // Disable the button
+  button.disabled = true;
+  // Get the selected voice from the dropdown menu
+  var voiceSelect = document.getElementById("voice-select");
+  var voice = voiceSelect.value;
+
+  // Use the SpeechSynthesis API to speak the text
+  var synth = window.speechSynthesis;
+  var utterance = new SpeechSynthesisUtterance(text);
+  // Set the voice based on the selected option
+  if (voice !== "default") {
+      utterance.voice = synth.getVoices().find(function(v) {
+          return v.lang === voice;
+      });
   }
-  
+  // Add an event listener to detect when speaking is done
+  utterance.addEventListener('end', function() {
+    // Enable the button when speaking is done
+    button.disabled = false;
+  });
+  synth.speak(utterance);
+}
+setTimeout(speak,100)
